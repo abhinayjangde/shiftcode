0082754edf8b8b7f3e62f2fd303e415e9bd2b876
Abhinay Jangde <abhinayjangde@gmail.com>
2025-05-17 08:13:57 +0530

    frontend work

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,30 @@
-import { useState } from "react"
-import {useForm} from "react-hook-form"
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers";
+import { Link } from "react-router-dom";
+import { Code, Eye, EyeOff, Loader2, Lock, Mail } from "lucide-react";
+import {z} from "zod"
 
-export default function Signup(){
+const signUpSchema = z.object({
+    email: z.string().email("Enter a valid email"),
+    password: z.string().min(6, "Password must be atleast 6 character."),
+    name: z.string().min(3, "Name must be at least 3 character.")
+})
 
-    return (
-        <div></div>
-    )
+export default function Signup() {
+    const [showPassword, setShowPassword] = useState(false)
+
+    const {
+        register,
+        handleSubmit,
+        formState: {error},
+
+    } = useForm({resolver: zodResolver(signUpSchema)})
+
+    const onSubmit = async (data)=>{
+        console.log(data)
+    }
+  return <div className="h-screen grid lg:grid-cols-2" >
+
+  </div>;
 }
